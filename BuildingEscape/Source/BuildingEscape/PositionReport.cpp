d59c50e6459ace0afb33d43ca4a87e23ee163d14
alkremn <alkremn@gmail.com>
2018-11-09 22:41:56 -0700

    added sec component

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -8,7 +9,9 @@ UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
+
 
 	// ...
 }
@@ -19,8 +22,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString objectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s!"), *objectName);
 }
 
 
